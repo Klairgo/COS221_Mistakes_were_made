20af2e02a48ccef400c53d5af195991a9b8e8610
Leviathan6331 <106149065+Leviathan6331@users.noreply.github.com>
2022-05-24 13:25:48 +0200

    125
    11

---
+++ php/header.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<meta>
+<title>Header</title>
+  <meta charset="UTF-8" />
+  <meta name="author" content="Mistakes were made" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</meta>
+<link rel="stylesheet" href="css/nav.css">
+</head>
+  <body>
+  <!--nav bar-->
+  <div class="topnav">
+  
+    <a href="index.php">Home</a>
+    <a href="today.php">Today</a>
+    <a href="world.php">World</a>
+    <a href="south-africa.php">South-Africa</a>
+    <a href="login.php">Login</a>
+    <a href="signup.php">Register</a>
+  </div>
+</body>
+
+
+
+</html>
+<?php include 'config.php';
+
+?>
