706f19c082bb58ff5cccc114b7e362a682772828
Alistair Ross <87852242+AlistairMRoss@users.noreply.github.com>
2022-05-24 11:37:52 +0200

    Add files via upload

---
+++ php/logout.php
@@ -0,0 +1,7 @@
+<?php
+    echo '<script type = "text/javascript">
+            localStorage.clear();
+            </script>';
+    echo 'You are logged out!<br>';
+    echo '<a href="index.php">Go Back</a>';
+?>
