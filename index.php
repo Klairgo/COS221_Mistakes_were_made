850a782a4bfd339133d50c6e3fe35e12aa2c2da0
Leviathan6331 <106149065+Leviathan6331@users.noreply.github.com>
2022-05-24 11:36:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,45 +1,52 @@
-
 <!DOCTYPE html>
 
 <html>
 
 <head>
-<meta>
-<title>Landing</title>
-  <meta charset="UTF-8" />
-  <meta name="author" content="Mistakes_were_made" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-</meta>
-<link rel="stylesheet" href="css/hero.css">
-<link rel="stylesheet" href="css/nav.css">
+     <meta>
+     <title>Landing</title>
+     <meta charset="UTF-8" />
+     <meta name="author" content="Mistakes_were_made" />
+     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+     </meta>
+     <link rel="stylesheet" href="css/hero.css">
+     <link rel="stylesheet" href="css/nav.css">
 </head>
+
 <body>
 
-<section class="hero">
+     <section class="hero">
 
-    <div class="hero-content">
-    
-       <h1 class="hero-title">
-            Discover the World Of E Sports
-       </h1>
-         
-       <!-- <h2 class="hero-subtitle">
+          <div class="hero-content">
+
+               <h1 class="hero-title">
+                    Discover the World Of E Sports
+               </h1>
+
+               <!-- <h2 class="hero-subtitle">
           
        </h2> -->
-         
-       <button type="button" class="hero-button" onclick="location.href='tours.html'">
-            Login &raquo;
-       </button>
-       <button type="button" class="hero-button" onclick="location.href='tours.html'">
-            Register &raquo;
-       </button>
-    
-    </div>
 
-</section>
+               <button type="button" id="log" class="hero-button" onclick="location.href='tours.html'">
+                    Login &raquo;
+               </button>
+               <button type="button" id="reg" class="hero-button" onclick="location.href='tours.html'">
+                    Register &raquo;
+               </button>
+               <script type="text/javascript">
+                    document.getElementById("log").onclick = function() {
+                         location.href = "php/loginP4.php";
+                    };
+                    document.getElementById("reg").onclick = function() {
+                         location.href = "php/signup.php";
+                    };
+               </script>
+
+          </div>
+
+     </section>
 
 
 </body>
 
 </html>
-
