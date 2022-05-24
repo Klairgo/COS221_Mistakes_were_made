e7042d7c34a2f055debb8c0eab74dfb567d18b68
Leviathan6331 <106149065+Leviathan6331@users.noreply.github.com>
2022-05-24 11:19:41 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<meta>
+<title>Landing</title>
+  <meta charset="UTF-8" />
+  <meta name="author" content="Mistakes_were_made" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</meta>
+<link rel="stylesheet" href="css/hero.css">
+<link rel="stylesheet" href="css/nav.css">
+</head>
+<body>
+
+<section class="hero">
+
+    <div class="hero-content">
+    
+       <h1 class="hero-title">
+            Discover the World Of E Sports
+       </h1>
+         
+       <!-- <h2 class="hero-subtitle">
+          
+       </h2> -->
+         
+       <button type="button" class="hero-button" onclick="location.href='tours.html'">
+            Login &raquo;
+       </button>
+       <button type="button" class="hero-button" onclick="location.href='tours.html'">
+            Register &raquo;
+       </button>
+    
+    </div>
+
+</section>
+
+
+</body>
+
+</html>
+
