2ee591d93142876007dac1702dc1f706c5755b07
Alistair Ross <87852242+AlistairMRoss@users.noreply.github.com>
2022-06-06 19:44:07 +0200

    Update headerForAdmin.php

--- php/headerForAdmin.php
+++ php/headerForAdmin.php
@@ -1,47 +1,54 @@
 <?php
-$state ="to";
+$state = "to";
 ?>
 <!DOCTYPE html>
 
 <html>
 
 <head>
-<meta>
-<title>Header</title>
+  <meta>
+  <title>Header</title>
   <meta charset="UTF-8" />
   <meta name="author" content="Mistakes were made" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-</meta>
-<link rel="stylesheet" href="../css/nav.css">
-<script src="../js/admin.js"></script>
+  </meta>
+  <link rel="stylesheet" href="../css/nav.css">
+  <script src="../js/admin.js"></script>
 </head>
-  <body>
+
+<body>
   <!--nav bar-->
-<div class="dropdown">
-  <button class="dropbtn">Create</button>
-  <div class="dropdown-content">
-    <a href="#">Team</a>
-    <a href="#"></a>
-    <a href="#"></a>
+  <div class="dropdown">
+    <button class="dropbtn">Create</button>
+    <div class="dropdown-content">
+      <a onclick="makeTeam()">Team</a>
+      <a onclick="makeTournament()">Tournament</a>
+      <a onclick="makePlayer()">Player</a>
+      <a onclick="makeVenue()">Venue</a>
+      <a onclick="makeMatch()">Match</a>
+      <a onclick="makeSponsor()">Sponsor</a>
+    </div>
   </div>
-</div>
 
-<div class="dropdown">
-  <button class="dropbtn">Update</button>
-  <div class="dropdown-content">
-    <a href="#">Team</a>
-    <a href="#"></a>
-    <a href="#"></a>
+  <div class="dropdown">
+    <button class="dropbtn">Update</button>
+    <div class="dropdown-content">
+      <a onclick="updateAccount()">Account</a>
+      <a onclick="updateManager()">Manager</a>
+      <a onclick="updateTeams()">Teams</a>
+      <a onclick="updateVenue()">Venue</a>
+      <a onclick="updateMatch()">Match</a>
+      <a onclick="updateSponsoredBy()">Sponsor</a>
+    </div>
   </div>
-</div>
 
-<div class="dropdown">
-  <button class="dropbtn">Delete</button>
-  <div class="dropdown-content">
-    <a href="#">Team</a>
-    <a href="#"></a>
-    <a href="#"></a>
+  <div class="dropdown">
+    <button class="dropbtn">Delete</button>
+    <div class="dropdown-content">
+      <a onclick="deleteccount()">Account</a>
+      <a onclick="deleteSponsor()">Sponsor</a>
+    </div>
   </div>
-</div>
 </body>
+
 </html>
