c760328018a7489bd8bf3ae68def92c2faf32480
Leviathan6331 <106149065+Leviathan6331@users.noreply.github.com>
2022-06-06 23:42:59 +0200

    tr
    t

--- php/playground.php
+++ php/playground.php
@@ -23,9 +23,72 @@ include_once("header.php");
     <section class="hero">
 
         <div class="hero-content" id="main">
-           <div class="flex-z">
-               
-           </div>
+            <div class="flex-z">
+                <div class="flex-1" id="gtag">
+                    Gamer Tag
+                </div>
+
+                <div class="flex-2" id="rest">
+                    <div class="flex-1" id="image">
+                        Image
+                    </div>
+
+                    <div class="flex-2" id="restin">
+                        <div class="flex-1" id="restin1">
+                            Player Name:
+                        </div>
+                        <div class="flex-1" id="restin1">
+                            Team name:
+                        </div>
+                        <div class="flex-1" id="restin1">
+                            Country:
+                        </div>
+
+
+                    </div>
+                </div>
+                <div class="flex-2" id="rest">
+                    <div class="flex-2" id="restin">
+                        <div class="flex-1" id="restin1">
+                            World Ranking:
+                        </div>
+                        <div class="flex-1" id="restin1">
+                            GAMES WON:
+                        </div>
+
+                        <div class="flex-1" id="restin1">
+                            Deaths:
+                        </div>
+                        <div class="flex-1" id="restin1">
+                            Assists:
+                        </div>
+                        <div class="flex-1" id="restin1">
+                            Games Played:
+                        </div>
+
+
+                    </div>
+
+                    <div class="flex-2" id="restin">
+                        <div class="flex-1" id="restin1">
+                            Utility Damage:
+                        </div>
+
+                        <div class="flex-1" id="restin1">
+                            ADR:
+                        </div>
+                        <div class="flex-1" id="restin1">
+                            Damage Done:
+                        </div>
+                        <div class="flex-1" id="restin1">
+                            Accuracy:
+                        </div>
+                        <div class="flex-1" id="restin1">
+                            Entry Success:
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
     </section>
