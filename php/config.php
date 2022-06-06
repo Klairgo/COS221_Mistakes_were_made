34fa28596ec364fe7f8befc17cc4db4f7bbd9e55
Leviathan6331 <106149065+Leviathan6331@users.noreply.github.com>
2022-06-06 19:20:23 +0200

    rtg
    ert

--- php/config.php
+++ php/config.php
@@ -2,7 +2,7 @@
 define("HOST", "wheatley.cs.up.ac.za");
 define("USER", "u21473103");
 define("PASS", "IHEDQVNUMXNLVVTZZUOCV4UPQL2N7LIN");
-define ("DB", "u21473103_216P3");
+define ("DB", "u21473103_221p5");
 function openCon(){
     $conn = new mysqli(HOST, USER, PASS, DB) or die("Connect failed: %5\n". $conn ->error);    
     return $conn;
