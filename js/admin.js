beca9a47f786b05cc9d3deb3061091728815249d
Alistair Ross <87852242+AlistairMRoss@users.noreply.github.com>
2022-06-06 19:55:07 +0200

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,65 @@
-function  create(){
-    
+function  makeTeam(){
+    ajax(
+        {
+            "action" : "get_tournament" ,
+            "team_id" :
+            "total_earnings" :
+            "name" :
+            "tournament" :
+            "manager_id" :
+            "ranking" :
+            "location" :
+        },
+}
+
+function makeTournament(){
+
+}
+
+function makePlayer(){
+
+}
+
+function makeVenue(){
+
+}
+
+function makeMatch(){
+
+}
+
+function makeSponsor(){
+
+}
+
+function updateAccount(){
+
+}
+
+function updateManager(){
+
+}
+
+function updateTeams(){
+
+}
+
+function updateVenue(){
+
+}
+
+function updateMatch(){
+
+}
+
+function updateSponsoredBy(){
+
+}
+
+function deleteccount(){
+
+}
+
+function deleteSponsor(){
+
 }
